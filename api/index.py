a952fcca1704f8085c4f8ae6ea5b0f1be55de6a2
Chitwan Bindal <chitwanbindal@Chitwans-MacBook-Air-2.local>
2024-09-02 17:28:28 +0530

    fix: hope this fixes

--- api/index.py
+++ api/index.py
@@ -4,39 +4,37 @@ import os
 from flask_cors import CORS
 from flask import Flask,jsonify,request
 
-    # create and configure the app
-    app = Flask(__name__) 
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
-    # CORS(app)
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# create and configure the app
+app = Flask(__name__) 
+# app.config.from_mapping(
+#     SECRET_KEY='dev',
+#     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+# )
+# CORS(app)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    @app.route('/')
-    def hello():
-        data ={
-            "response": "true"
-        }
-        return jsonify(data)
+@app.route('/')
+def hello():
+    data ={
+        "response": "true"
+    }
+    return jsonify(data)
 
-    @app.route('/calculate',methods=['POST'])
-    def calculateMove():
-        data = request.json
-        boardData:list[list[str]] = data.get('boardData')
-        check = data.get('check')
-        # print(boardData)
-        head = chessengine.__init__(boardData,check)
-        newBoard = head.alphabeta()
-        # print(head.board.movePlayed)
-        data = {
-            "movePlayedByAI": newBoard.movePlayed
-        }
-        return jsonify(data)
-
-
+@app.route('/calculate',methods=['POST'])
+def calculateMove():
+    data = request.json
+    boardData:list[list[str]] = data.get('boardData')
+    check = data.get('check')
+    # print(boardData)
+    head = chessengine.__init__(boardData,check)
+    newBoard = head.alphabeta()
+    # print(head.board.movePlayed)
+    data = {
+        "movePlayedByAI": newBoard.movePlayed
+    }
+    return jsonify(data)
 
