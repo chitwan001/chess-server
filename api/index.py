e2d4a7bc4f5e374c8b6bc830fb017c08495dd0b9
Chitwan Bindal <chitwanbindal@Chitwans-MacBook-Air-2.local>
2024-09-02 16:52:06 +0530

    fix: hope this fixes

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import os
 
 from flask_cors import CORS
 from flask import Flask,jsonify,request
-def create_app():
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     # app.config.from_mapping(
@@ -38,7 +38,5 @@ def create_app():
         }
         return jsonify(data)
 
-    return app
 
 
-create_app()
